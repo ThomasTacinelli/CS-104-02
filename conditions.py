286152f9c86d397fe1bcfebc5f30d1d898cedb40
ThomasTacinelli <71794465+ThomasTacinelli@users.noreply.github.com>
2020-10-12 13:48:38 -0400

    Add files via upload
    conditions

---
+++ conditions.py
@@ -0,0 +1,12 @@
+#Initialize all variables
+temp = int(input("Please enter the current temperature: "))
+if (temp >90):
+        print("wear shorts")
+elif (temp >70):
+        print("short sleeves are fine")
+elif (temp >50):
+        print("wear a jacket")
+elif (temp >32):
+        print("wear a heavy coat")
+else:
+        print("stay inside")
